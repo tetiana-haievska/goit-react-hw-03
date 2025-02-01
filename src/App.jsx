59d9a814fa81ec36d0b6e889af9417f96ae43279
Tetiana <tatiana.makarenko94@gmail.com>
2025-02-01 20:39:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -36,20 +36,21 @@
 
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
-import ContactForm from "./components/ContactForm/ContactForm.jsx";
+import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
-
-const App = () => {
+export default function App() {
   const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ];
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+          { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+          { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+          { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+        ];
   });
 
   const [filter, setFilter] = useState("");
@@ -63,22 +64,29 @@ const App = () => {
     setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
-  const deleteContact = (id) => {
-    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter(({ id }) => id !== contactId)
+    );
   };
 
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
-    <div >
+    <div>
       <h1>Phonebook</h1>
       <ContactForm onAddContact={addContact} />
-      <SearchBox filter={filter} setFilter={setFilter} />
-      <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
+      <SearchBox value={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
     </div>
   );
-};
-
-export default App;
+}
